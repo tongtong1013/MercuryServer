dd7f7d5838cfdf23794543e3e7e048d805b22128
tongtong1013 <banzhiluolan@163.com>
2017-08-15 23:42:20 +0800

    complete checking id and password.

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
+header('Content-Type:application/json;charset=utf8');
+$id = $_POST['username'];
+$password = $_POST['password'];
+//echo $id;
+//echo $password;
+
+$filetemp = dirname(__FILE__) . '/ConfigFile/TempInfo.txt';
+
+//file_put_contents($filetemp, json_encode($id + " and " + $password));
+
 $filename = dirname(__FILE__) . '/ConfigFile/ServerInfo.txt';
 if(!is_file($filename))
 {
@@ -20,17 +30,42 @@ $dbData = array(
 //echo $dbData['user'];
 //echo $dbData['password'];
 
-$db = mysql_connect($dbData['host'],$dbData['user'],$dbData['password']);
+$db = mysqli_connect($dbData['host'],$dbData['user'],$dbData['password']);
 if(!$db)
 {
-    die("Could not connect!" . mysql_error());
+    die("Could not connect!" . mysqli_error());
 }
-mysql_select_db("mercury",$db);
-$result = mysql_query("SELECT * FROM tbl_idpassword");
+mysqli_select_db($db,"mercury");
+$result = mysqli_query($db,"SELECT * FROM tbl_idpassword WHERE ID = '" . $id . "' AND Password = '" . $password . "'");
+
+$answerYes = array(
+    'yes' => 1
+);
+$answerNo = array(
+    'no' => 0
+);
+$jsonStrYse = json_encode($answerYes);
+$jsonStrNo = json_encode($answerNo);
+$answer = array(
+    'answer' => 0
+);
+if(mysqli_num_rows($result)>0)
+{
+	$answer['answer'] = 1;
+}
+else
+{
+    $answer['answer'] = 0;
+}
+$jsonStr = json_encode($answer);
+echo $jsonStr;
+/*
 while($row = mysql_fetch_array($result))
 {
     echo $row['ID'] . " " . $row['Password'];
     echo "<br />";
 }
-mysql_close($con);
+*/
+mysqli_close($db);
+
 ?>
